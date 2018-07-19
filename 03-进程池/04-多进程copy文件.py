b0f2b2182e32deaafdeb62cad5768948b9a6300e
zhongxin <490336534@qq.com>
2018-07-19 22:19:01 +0800

    修改输出当前进度的语句

--- 03-进程池/04-多进程copy文件.py
+++ 03-进程池/04-多进程copy文件.py
@@ -1,9 +1,7 @@
 #把一个文件夹里面的文件全部copy到一个新的文件夹
 # 1.获取文件夹所有文件名
-
 import os,time
 from multiprocessing import Pool,Manager
-
 def copy_file(filename,old_name,new_name,q):
     with open(old_name+'/'+filename,encoding='utf-8',errors='ignore') as file:
         content = file.read()
@@ -11,7 +9,6 @@ def copy_file(filename,old_name,new_name,q):
 
     with open(new_name+'/'+filename,'w',encoding='utf-8') as f_w:
         f_w.write(content)
-
 def main():
     old_name = input('请输入要复制的文件夹的名字：')
     new_name = input('请输入要copy的文件夹的名字:')
@@ -21,20 +18,25 @@ def main():
     filenames = os.listdir(old_name) #获取当前文件夹中的文件名
     pool = Pool()
     q = Manager().Queue()
+    total = len(filenames)
+    print('总文件数是：%s' % total)
     for name in filenames:
         pool.apply_async(copy_file,args=(name,old_name,new_name,q))
     # pool.map(copy_file,filenames)
     # pool.close()
     # pool.join()
     num = q.qsize()
-    total = len(filenames)
-    print('总文件数是：%s'%total)
     while num < total:
+        num1 = q.qsize()
         num = q.qsize()
-        print('\r当前复制进度%.2f%%,num是%d'%(float((num/total)*100),num))
+        if num != num1:
+            print('\r当前复制进度%.2f%%,num是%d'%(float((num/total)*100),num))
+        if num == total:
+            print('复制完成')
 
 if __name__ == '__main__':
     main()
+
 
 
 
